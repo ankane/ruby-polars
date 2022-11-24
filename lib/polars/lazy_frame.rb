f9608b6464e2a609b3dd593be54d6e5b984ceae9
Andrew Kane <andrew@ankane.org>
2022-11-23 16:58:46 -0800

    Added more method placeholders [skip ci]

--- lib/polars/lazy_frame.rb
+++ lib/polars/lazy_frame.rb
@@ -8,6 +8,51 @@ module Polars
       ldf
     end
 
+    # def columns
+    # end
+
+    # def dtypes
+    # end
+
+    # def schema
+    # end
+
+    # def width
+    # end
+
+    # def include?(key)
+    # end
+
+    # clone handled by initialize_copy
+
+    # def [](item)
+    # end
+
+    # def to_s
+    # end
+    # alias_method :inspect, :to_s
+
+    # def write_json
+    # end
+
+    # def pipe
+    # end
+
+    # def describe_plan
+    # end
+
+    # def describe_optimized_plan
+    # end
+
+    # def show_graph
+    # end
+
+    # def sort
+    # end
+
+    # def profile
+    # end
+
     def collect(
       type_coercion: true,
       predicate_pushdown: true,
@@ -42,6 +87,19 @@ module Polars
       Utils.wrap_df(ldf.collect)
     end
 
+    # def fetch
+    # end
+
+    def lazy
+      self
+    end
+
+    # def cache
+    # end
+
+    # def cleared
+    # end
+
     def filter(predicate)
       _from_rbldf(
         _ldf.filter(
@@ -60,6 +118,15 @@ module Polars
       lgb = _ldf.groupby(rbexprs_by, maintain_order)
       LazyGroupBy.new(lgb, self.class)
     end
+
+    # def groupby_rolling
+    # end
+
+    # def groupby_dynamic
+    # end
+
+    # def join_asof
+    # end
 
     def join(
       other,
@@ -132,15 +199,57 @@ module Polars
       _from_rbldf(_ldf.with_columns(rbexprs))
     end
 
+    # def with_context
+    # end
+
     def with_column(column)
       with_columns([column])
     end
+
+    # def drop
+    # end
 
     def rename(mapping)
       existing = mapping.keys
       _new = mapping.values
       _from_rbldf(_ldf.rename(existing, _new))
     end
+
+    # def reverse
+    # end
+
+    # def shift
+    # end
+
+    # def shift_and_fill
+    # end
+
+    # def slice
+    # end
+
+    # def limit
+    # end
+
+    # def head
+    # end
+
+    # def tail
+    # end
+
+    # def last
+    # end
+
+    # def first
+    # end
+
+    # def with_row_count
+    # end
+
+    # def take_every
+    # end
+
+    # def fill_null
+    # end
 
     def fill_nan(fill_value)
       if !fill_value.is_a?(Expr)
@@ -149,7 +258,57 @@ module Polars
       _from_rbldf(_ldf.fill_nan(fill_value._rbexpr))
     end
 
+    # def std
+    # end
+
+    # def var
+    # end
+
+    # def max
+    # end
+
+    # def min
+    # end
+
+    # def sum
+    # end
+
+    # def mean
+    # end
+
+    # def median
+    # end
+
+    # def quantile
+    # end
+
+    # def explode
+    # end
+
+    # def unique
+    # end
+
+    # def drop_nulls
+    # end
+
+    # def melt
+    # end
+
+    # def map
+    # end
+
+    # def interpolate
+    # end
+
+    # def unnest
+    # end
+
     private
+
+    def initialize_copy(other)
+      super
+      self._ldf = _df._clone
+    end
 
     def _from_rbldf(rb_ldf)
       self.class._from_rbldf(rb_ldf)
