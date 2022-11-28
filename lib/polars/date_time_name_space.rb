184223293fe84eb04f09af6208f753f5e3c8b3d2
Andrew Kane <andrew@ankane.org>
2022-11-28 02:30:14 -0800

    Added more methods to DateTimeNameSpace [skip ci]

--- lib/polars/date_time_name_space.rb
+++ lib/polars/date_time_name_space.rb
@@ -80,5 +80,97 @@ module Polars
       out = s.median.to_i
       Utils._to_ruby_datetime(out, s.dtype, tu: s.time_unit)
     end
+
+    # Return mean as Ruby object.
+    #
+    # @return [Object]
+    #
+    # @example
+    #   date = Polars.date_range(DateTime.new(2001, 1, 1), DateTime.new(2001, 1, 3), "1d")
+    #   # =>
+    #   # shape: (3,)
+    #   # Series: '' [datetime[μs]]
+    #   # [
+    #   #         2001-01-01 00:00:00
+    #   #         2001-01-02 00:00:00
+    #   #         2001-01-03 00:00:00
+    #   # ]
+    #
+    # @example
+    #   date.dt.mean
+    #   # => 2001-01-02 00:00:00 UTC
+    def mean
+      s = Utils.wrap_s(_s)
+      out = s.mean.to_i
+      Utils._to_ruby_datetime(out, s.dtype, tu: s.time_unit)
+    end
+
+    # Format Date/datetime with a formatting rule.
+    #
+    # See [chrono strftime/strptime](https://docs.rs/chrono/0.4.19/chrono/format/strftime/index.html).
+    #
+    # @return [Series]
+    #
+    # @example
+    #   start = DateTime.new(2001, 1, 1)
+    #   stop = DateTime.new(2001, 1, 4)
+    #   date = Polars.date_range(start, stop, "1d")
+    #   # =>
+    #   # shape: (4,)
+    #   # Series: '' [datetime[μs]]
+    #   # [
+    #   #         2001-01-01 00:00:00
+    #   #         2001-01-02 00:00:00
+    #   #         2001-01-03 00:00:00
+    #   #         2001-01-04 00:00:00
+    #   # ]
+    #
+    # @example
+    #   date.dt.strftime("%Y-%m-%d")
+    #   # =>
+    #   # shape: (4,)
+    #   # Series: '' [str]
+    #   # [
+    #   #         "2001-01-01"
+    #   #         "2001-01-02"
+    #   #         "2001-01-03"
+    #   #         "2001-01-04"
+    #   # ]
+    def strftime(fmt)
+      super
+    end
+
+    # Extract the year from the underlying date representation.
+    #
+    # Applies to Date and Datetime columns.
+    #
+    # Returns the year number in the calendar date.
+    #
+    # @return [Series]
+    #
+    # @example
+    #   start = DateTime.new(2001, 1, 1)
+    #   stop = DateTime.new(2002, 1, 1)
+    #   date = Polars.date_range(start, stop, "1y")
+    #   # =>
+    #   # shape: (2,)
+    #   # Series: '' [datetime[μs]]
+    #   # [
+    #   #         2001-01-01 00:00:00
+    #   #         2002-01-01 00:00:00
+    #   # ]
+    #
+    # @example
+    #   date.dt.year
+    #   # =>
+    #   # shape: (2,)
+    #   # Series: '' [i32]
+    #   # [
+    #   #         2001
+    #   #         2002
+    #   # ]
+    def year
+      super
+    end
   end
 end
