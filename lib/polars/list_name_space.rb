184501a67370be60c26ad46bddee42ab0eed8c87
Andrew Kane <andrew@ankane.org>
2022-11-28 01:19:24 -0800

    Documented more methods [skip ci]

--- lib/polars/list_name_space.rb
+++ lib/polars/list_name_space.rb
@@ -16,6 +16,122 @@ module Polars
       self._s = series._s
     end
 
+    # Get the length of the arrays as UInt32.
+    #
+    # @return [Series]
+    #
+    # @example
+    #   s = Polars::Series.new([[1, 2, 3], [5]])
+    #   s.arr.lengths
+    #   # =>
+    #   # shape: (2,)
+    #   # Series: '' [u32]
+    #   # [
+    #   #         3
+    #   #         1
+    #   # ]
+    def lengths
+      super
+    end
+
+    # Sum all the arrays in the list.
+    #
+    # @return [Series]
+    def sum
+      super
+    end
+
+    # Compute the max value of the arrays in the list.
+    #
+    # @return [Series]
+    def max
+      super
+    end
+
+    # Compute the min value of the arrays in the list.
+    #
+    # @return [Series]
+    def min
+      super
+    end
+
+    # Compute the mean value of the arrays in the list.
+    #
+    # @return [Series]
+    def mean
+      super
+    end
+
+    # Sort the arrays in the list.
+    #
+    # @return [Series]
+    def sort(reverse: false)
+      super
+    end
+
+    # Reverse the arrays in the list.
+    #
+    # @return [Series]
+    def reverse
+      super
+    end
+
+    # Get the unique/distinct values in the list.
+    #
+    # @return [Series]
+    def unique
+      super
+    end
+
+    # Concat the arrays in a Series dtype List in linear time.
+    #
+    # @param other [Object]
+    #   Columns to concat into a List Series
+    #
+    # @return [Series]
+    def concat(other)
+      super
+    end
+
+    # Get the value by index in the sublists.
+    #
+    # So index `0` would return the first item of every sublist
+    # and index `-1` would return the last item of every sublist
+    # if an index is out of bounds, it will return a `None`.
+    #
+    # @param index [Integer]
+    #   Index to return per sublist
+    #
+    # @return [Series]
+    def get(index)
+      super
+    end
+
+    # Get the value by index in the sublists.
+    #
+    # @return [Series]
+    def [](item)
+      get(item)
+    end
+
+    # def join
+    # end
+
+    # def first
+    # end
+
+    # def last
+    # end
+
+    # def contains
+    # end
+
+    # def arg_min
+    # end
+
+    # def arg_max
+    # end
+
     # Calculate the n-th discrete difference of every sublist.
     #
     # @param n [Integer]
@@ -125,5 +241,11 @@ module Polars
     def tail(n = 5)
       super
     end
+
+    # def to_struct
+    # end
+
+    # def eval
+    # end
   end
 end
