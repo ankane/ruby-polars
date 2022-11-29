2624073d9c5633c9efeac21cb482ad9113d1267a
Andrew Kane <andrew@ankane.org>
2022-11-29 00:32:48 -0800

    Added rolling_sum method [skip ci]

--- lib/polars/series.rb
+++ lib/polars/series.rb
@@ -2424,8 +2424,55 @@ module Polars
         .to_series
     end
 
-    # def rolling_sum
-    # end
+    # Apply a rolling sum (moving sum) over the values in this array.
+    #
+    # A window of length `window_size` will traverse the array. The values that fill
+    # this window will (optionally) be multiplied with the weights given by the
+    # `weight` vector. The resulting values will be aggregated to their sum.
+    #
+    # @param window_size [Integer]
+    #   The length of the window.
+    # @param weights [Array]
+    #   An optional slice with the same length as the window that will be multiplied
+    #   elementwise with the values in the window.
+    # @param min_periods [Integer]
+    #   The number of values in the window that should be non-null before computing
+    #   a result. If None, it will be set equal to window size.
+    # @param center [Boolean]
+    #   Set the labels at the center of the window
+    #
+    # @return [Series]
+    #
+    # @example
+    #   s = Polars::Series.new("a", [1, 2, 3, 4, 5])
+    #   s.rolling_sum(2)
+    #   # =>
+    #   # shape: (5,)
+    #   # Series: 'a' [i64]
+    #   # [
+    #   #         null
+    #   #         3
+    #   #         5
+    #   #         7
+    #   #         9
+    #   # ]
+    def rolling_sum(
+      window_size,
+      weights: nil,
+      min_periods: nil,
+      center: false
+    )
+      to_frame
+        .select(
+          Polars.col(name).rolling_sum(
+            window_size,
+            weights: weights,
+            min_periods: min_periods,
+            center: center
+          )
+        )
+        .to_series
+    end
 
     # def rolling_std
     # end
