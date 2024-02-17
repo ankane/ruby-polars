adf854bcac11c37c1e66af5bdc40b8c2cb8a1d31
Andrew Kane <andrew@ankane.org>
2024-02-16 21:43:35 -0800

    Added more methods to MetaExpr [skip ci]

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,6 +8,7 @@
 - Added `merge_sorted` method to `DataFrame` and `LazyFrame`
 - Added more methods to `ArrayExpr` and `ArrayNameSpace`
 - Added more methods to `ListExpr` and `ListNameSpace`
+- Added more methods to `MetaExpr`
 - Added `schema_overrides` option to `read_database` method
 - Aliased `cleared` to `clear` for `DataFrame` and `LazyFrame`
 - Fixed error with `BigDecimal` objects
--- lib/polars/meta_expr.rb
+++ lib/polars/meta_expr.rb
@@ -23,6 +23,87 @@ module Polars
       !(self == other)
     end
 
+    # Indicate if this expression is the same as another expression.
+    #
+    # @return [Boolean]
+    #
+    # @example
+    #   foo_bar = Polars.col("foo").alias("bar")
+    #   foo = Polars.col("foo")
+    #   foo_bar.meta.eq(foo)
+    #   # => false
+    #   foo_bar2 = Polars.col("foo").alias("bar")
+    #   foo_bar.meta.eq(foo_bar2)
+    #   # => true
+    def eq(other)
+      _rbexpr.meta_eq(other._rbexpr)
+    end
+
+    # Indicate if this expression is NOT the same as another expression.
+    #
+    # @return [Boolean]
+    #
+    # @example
+    #   foo_bar = Polars.col("foo").alias("bar")
+    #   foo = Polars.col("foo")
+    #   foo_bar.meta.ne(foo)
+    #   # => true
+    #   foo_bar2 = Polars.col("foo").alias("bar")
+    #   foo_bar.meta.ne(foo_bar2)
+    #   # => false
+    def ne(other)
+      !eq(other)
+    end
+
+    # Indicate if this expression expands into multiple expressions.
+    #
+    # @return [Boolean]
+    #
+    # @example
+    #   e = Polars.col(["a", "b"]).alias("bar")
+    #   e.meta.has_multiple_outputs
+    #   # => true
+    def has_multiple_outputs
+      _rbexpr.meta_has_multiple_outputs
+    end
+
+    # Indicate if this expression is a basic (non-regex) unaliased column.
+    #
+    # @return [Boolean]
+    #
+    # @example
+    #   e = Polars.col("foo")
+    #   e.meta.is_column
+    #   # => true
+    #   e = Polars.col("foo") * Polars.col("bar")
+    #   e.meta.is_column
+    #   # => false
+    #   e = Polars.col("^col.*\d+$")
+    #   e.meta.is_column
+    #   # => false
+    def is_column
+      _rbexpr.meta_is_column
+    end
+
+    # Indicate if this expression expands to columns that match a regex pattern.
+    #
+    # @return [Boolean]
+    #
+    # @example
+    #   e = Polars.col("^.*$").alias("bar")
+    #   e.meta.is_regex_projection
+    #   # => true
+    def is_regex_projection
+      _rbexpr.meta_is_regex_projection
+    end
+
+    # Get the column name that this expression would produce.
+    #
+    # @return [String]
+    def output_name
+      _rbexpr.meta_output_name
+    end
+
     # Pop the latest expression and return the input(s) of the popped expression.
     #
     # @return [Array]
@@ -35,13 +116,6 @@ module Polars
     # @return [Array]
     def root_names
       _rbexpr.meta_roots
-    end
-
-    # Get the column name that this expression would produce.
-    #
-    # @return [String]
-    def output_name
-      _rbexpr.meta_output_name
     end
 
     # Undo any renaming operation like `alias` or `keep_name`.
