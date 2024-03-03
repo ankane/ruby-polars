08c6a29f0261bc63da5fc6b28397df07f8a09790
Andrew Kane <andrew@ankane.org>
2024-03-02 18:52:16 -0800

    Improved tests for Testing module [skip ci]

--- test/testing_test.rb
+++ test/testing_test.rb
@@ -4,6 +4,29 @@ class TestingTest < Minitest::Test
   def test_assert_frame_equal
     df = Polars::DataFrame.new({"a" => [1, 2, 3]})
     assert_frame_equal df, df
+  end
+
+  def test_assert_frame_equal_different_dtype
+    df = Polars::DataFrame.new({"a" => [1, 2, 3]})
+    df2 = Polars::DataFrame.new({"a" => [1.0, 2.0, 3.0]})
+    error = assert_raises(Polars::AssertionError) do
+      assert_frame_equal df, df2
+    end
+    assert_match "DataFrames are different (dtypes do not match)", error.message
+    assert_frame_equal df, df2, check_dtype: false
+  end
+
+  def test_assert_frame_equal_different_columns
+    df = Polars::DataFrame.new({"a" => [1, 2, 3]})
+    df2 = Polars::DataFrame.new({"b" => [1, 2, 3]})
+    error = assert_raises(Polars::AssertionError) do
+      assert_frame_equal df, df2
+    end
+    assert_equal "columns [\"b\"] in left DataFrames, but not in right", error.message
+  end
+
+  def test_assert_frame_not_equal
+    df = Polars::DataFrame.new({"a" => [1, 2, 3]})
     error = assert_raises(Polars::AssertionError) do
       assert_frame_not_equal df, df
     end
@@ -11,8 +34,12 @@ class TestingTest < Minitest::Test
   end
 
   def test_assert_series_equal
-    s = Polars::Series.new([1.0, Float::NAN, Float::INFINITY])
+    s = Polars::Series.new([1, 2, 3])
     assert_series_equal s, s
+  end
+
+  def test_assert_series_not_equal
+    s = Polars::Series.new([1, 2, 3])
     error = assert_raises(Polars::AssertionError) do
       assert_series_not_equal s, s
     end
