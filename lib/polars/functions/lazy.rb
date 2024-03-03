67877c4b47a797cdc01edab97d8692cbd3b4f5a3
Andrew Kane <andrew@ankane.org>
2024-03-03 00:37:36 -0800

    Moved more methods [skip ci]

--- lib/polars/functions/lazy.rb
+++ lib/polars/functions/lazy.rb
@@ -696,40 +696,6 @@ module Polars
       end
     end
 
-    # Compute the spearman rank correlation between two columns.
-    #
-    # Missing data will be excluded from the computation.
-    #
-    # @param a [Object]
-    #   Column name or Expression.
-    # @param b [Object]
-    #   Column name or Expression.
-    # @param ddof [Integer]
-    #   Delta degrees of freedom
-    # @param propagate_nans [Boolean]
-    #   If `True` any `NaN` encountered will lead to `NaN` in the output.
-    #   Defaults to `False` where `NaN` are regarded as larger than any finite number
-    #   and thus lead to the highest rank.
-    #
-    # @return [Expr]
-    def spearman_rank_corr(a, b, ddof: 1, propagate_nans: false)
-      corr(a, b, method: "spearman", ddof: ddof, propagate_nans: propagate_nans)
-    end
-
-    # Compute the pearson's correlation between two columns.
-    #
-    # @param a [Object]
-    #   Column name or Expression.
-    # @param b [Object]
-    #   Column name or Expression.
-    # @param ddof [Integer]
-    #   Delta degrees of freedom
-    #
-    # @return [Expr]
-    def pearson_corr(a, b, ddof: 1)
-      corr(a, b, method: "pearson", ddof: ddof)
-    end
-
     # Compute the covariance between two columns/ expressions.
     #
     # @param a [Object]
