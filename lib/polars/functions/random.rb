7c4b120589f3c52b0d3d14db263be9001083a230
Andrew Kane <andrew@ankane.org>
2024-03-03 00:10:44 -0800

    Added set_random_seed method [skip ci]

---
+++ lib/polars/functions/random.rb
@@ -0,0 +1,16 @@
+module Polars
+  module Functions
+    # Set the global random seed for Polars.
+    #
+    # This random seed is used to determine things such as shuffle ordering.
+    #
+    # @param seed [Integer]
+    #   A non-negative integer < 2**64 used to seed the internal global
+    #   random number generator.
+    #
+    # @return [Object]
+    def set_random_seed(seed)
+      Plr.set_random_seed(seed)
+    end
+  end
+end
