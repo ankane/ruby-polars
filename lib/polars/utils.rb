5252f29a1e2a4159673e8451a880dbb59c032a55
Andrew Kane <andrew@ankane.org>
2024-06-23 22:44:58 -0700

    Moved more functions [skip ci]

--- lib/polars/utils.rb
+++ lib/polars/utils.rb
@@ -20,56 +20,6 @@ module Polars
       nil
     end
 
-    def self._to_ruby_time(value)
-      if value == 0
-        ::Time.utc(2000, 1, 1)
-      else
-        seconds, nanoseconds = value.divmod(1_000_000_000)
-        minutes, seconds = seconds.divmod(60)
-        hours, minutes = minutes.divmod(60)
-        ::Time.utc(2000, 1, 1, hours, minutes, seconds, nanoseconds / 1000.0)
-      end
-    end
-
-    def self._to_ruby_duration(value, time_unit = "ns")
-      if time_unit == "ns"
-        value / 1e9
-      elsif time_unit == "us"
-        value / 1e6
-      elsif time_unit == "ms"
-        value / 1e3
-      else
-        raise ArgumentError, "time_unit must be one of {{'ns', 'us', 'ms'}}, got #{time_unit}"
-      end
-    end
-
-    def self._to_ruby_date(value)
-      # days to seconds
-      # important to create from utc. Not doing this leads
-      # to inconsistencies dependent on the timezone you are in.
-      ::Time.at(value * 86400).utc.to_date
-    end
-
-    def self._to_ruby_datetime(value, time_unit = "ns", time_zone = nil)
-      if time_zone.nil? || time_zone == ""
-        if time_unit == "ns"
-          ::Time.at(value / 1000000000, value % 1000000000, :nsec).utc
-        elsif time_unit == "us"
-          ::Time.at(value / 1000000, value % 1000000, :usec).utc
-        elsif time_unit == "ms"
-          ::Time.at(value / 1000, value % 1000, :millisecond).utc
-        else
-          raise ArgumentError, "time_unit must be one of {{'ns', 'us', 'ms'}}, got #{time_unit}"
-        end
-      else
-        raise Todo
-      end
-    end
-
-    def self._to_ruby_decimal(digits, scale)
-      BigDecimal("#{digits}e#{scale}")
-    end
-
     def self.selection_to_rbexpr_list(exprs)
       if exprs.is_a?(::String) || exprs.is_a?(Symbol) || exprs.is_a?(Expr) || exprs.is_a?(Series)
         exprs = [exprs]
