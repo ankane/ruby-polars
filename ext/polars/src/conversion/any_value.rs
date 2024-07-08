141a0d81d7550002006fa1ad44fcf2371a4ca9e9
Andrew Kane <andrew@ankane.org>
2024-07-07 21:11:13 -0700

    Synced code [skip ci]

--- ext/polars/src/conversion/any_value.rs
+++ ext/polars/src/conversion/any_value.rs
@@ -17,6 +17,12 @@ impl IntoValue for Wrap<AnyValue<'_>> {
     }
 }
 
+impl<'s> TryConvert for Wrap<AnyValue<'s>> {
+    fn try_convert(ob: Value) -> RbResult<Self> {
+        rb_object_to_any_value(ob, true).map(Wrap)
+    }
+}
+
 pub(crate) fn any_value_into_rb_object(av: AnyValue, ruby: &Ruby) -> Value {
     match av {
         AnyValue::UInt8(v) => ruby.into_value(v),
@@ -74,100 +80,98 @@ pub(crate) fn any_value_into_rb_object(av: AnyValue, ruby: &Ruby) -> Value {
     }
 }
 
-impl<'s> TryConvert for Wrap<AnyValue<'s>> {
-    fn try_convert(ob: Value) -> RbResult<Self> {
-        if ob.is_kind_of(class::true_class()) || ob.is_kind_of(class::false_class()) {
-            Ok(AnyValue::Boolean(bool::try_convert(ob)?).into())
-        } else if let Some(v) = Integer::from_value(ob) {
-            Ok(AnyValue::Int64(v.to_i64()?).into())
-        } else if let Some(v) = Float::from_value(ob) {
-            Ok(AnyValue::Float64(v.to_f64()).into())
-        } else if let Some(v) = RString::from_value(ob) {
-            if v.enc_get() == Index::utf8() {
-                Ok(AnyValue::StringOwned(v.to_string()?.into()).into())
-            } else {
-                Ok(AnyValue::BinaryOwned(unsafe { v.as_slice() }.to_vec()).into())
-            }
-        // call is_a? for ActiveSupport::TimeWithZone
-        } else if ob.funcall::<_, _, bool>("is_a?", (class::time(),))? {
-            let sec = ob.funcall::<_, _, i64>("to_i", ())?;
-            let nsec = ob.funcall::<_, _, i64>("nsec", ())?;
-            let v = sec * 1_000_000_000 + nsec;
-            // TODO support time zone when possible
-            // https://github.com/pola-rs/polars/issues/9103
-            Ok(AnyValue::Datetime(v, TimeUnit::Nanoseconds, &None).into())
-        } else if ob.is_nil() {
-            Ok(AnyValue::Null.into())
-        } else if let Some(dict) = RHash::from_value(ob) {
-            let len = dict.len();
-            let mut keys = Vec::with_capacity(len);
-            let mut vals = Vec::with_capacity(len);
-            dict.foreach(|k: Value, v: Value| {
-                let key = String::try_convert(k)?;
-                let val = Wrap::<AnyValue>::try_convert(v)?.0;
-                let dtype = DataType::from(&val);
-                keys.push(Field::new(&key, dtype));
-                vals.push(val);
-                Ok(ForEach::Continue)
-            })?;
-            Ok(Wrap(AnyValue::StructOwned(Box::new((vals, keys)))))
-        } else if let Some(v) = RArray::from_value(ob) {
-            if v.is_empty() {
-                Ok(Wrap(AnyValue::List(Series::new_empty("", &DataType::Null))))
-            } else {
-                let list = v;
-
-                let mut avs = Vec::with_capacity(25);
-                let mut iter = list.into_iter();
-
-                for item in (&mut iter).take(25) {
-                    avs.push(Wrap::<AnyValue>::try_convert(item)?.0)
-                }
-
-                let (dtype, _n_types) =
-                    any_values_to_supertype_and_n_dtypes(&avs).map_err(RbPolarsErr::from)?;
-
-                // push the rest
-                avs.reserve(list.len());
-                for item in iter {
-                    avs.push(Wrap::<AnyValue>::try_convert(item)?.0)
-                }
-
-                let s = Series::from_any_values_and_dtype("", &avs, &dtype, true)
-                    .map_err(RbPolarsErr::from)?;
-                Ok(Wrap(AnyValue::List(s)))
-            }
-        } else if ob.is_kind_of(crate::rb_modules::datetime()) {
-            let sec: i64 = ob.funcall("to_i", ())?;
-            let nsec: i64 = ob.funcall("nsec", ())?;
-            Ok(Wrap(AnyValue::Datetime(
-                sec * 1_000_000_000 + nsec,
-                TimeUnit::Nanoseconds,
-                &None,
-            )))
-        } else if ob.is_kind_of(crate::rb_modules::date()) {
-            // convert to DateTime for UTC
-            let v = ob
-                .funcall::<_, _, Value>("to_datetime", ())?
-                .funcall::<_, _, Value>("to_time", ())?
-                .funcall::<_, _, i64>("to_i", ())?;
-            Ok(Wrap(AnyValue::Date((v / 86400) as i32)))
-        } else if ob.is_kind_of(crate::rb_modules::bigdecimal()) {
-            let (sign, digits, _, exp): (i8, String, i32, i32) = ob.funcall("split", ()).unwrap();
-            let (mut v, scale) = abs_decimal_from_digits(digits, exp).ok_or_else(|| {
-                RbPolarsErr::other("BigDecimal is too large to fit in Decimal128".into())
-            })?;
-            if sign < 0 {
-                // TODO better error
-                v = v.checked_neg().unwrap();
-            }
-            Ok(Wrap(AnyValue::Decimal(v, scale)))
+pub(crate) fn rb_object_to_any_value<'s>(ob: Value, _strict: bool) -> RbResult<AnyValue<'s>> {
+    if ob.is_kind_of(class::true_class()) || ob.is_kind_of(class::false_class()) {
+        Ok(AnyValue::Boolean(bool::try_convert(ob)?))
+    } else if let Some(v) = Integer::from_value(ob) {
+        Ok(AnyValue::Int64(v.to_i64()?))
+    } else if let Some(v) = Float::from_value(ob) {
+        Ok(AnyValue::Float64(v.to_f64()))
+    } else if let Some(v) = RString::from_value(ob) {
+        if v.enc_get() == Index::utf8() {
+            Ok(AnyValue::StringOwned(v.to_string()?.into()))
         } else {
-            Err(RbPolarsErr::other(format!(
-                "object type not supported {:?}",
-                ob
-            )))
+            Ok(AnyValue::BinaryOwned(unsafe { v.as_slice() }.to_vec()))
         }
+    // call is_a? for ActiveSupport::TimeWithZone
+    } else if ob.funcall::<_, _, bool>("is_a?", (class::time(),))? {
+        let sec = ob.funcall::<_, _, i64>("to_i", ())?;
+        let nsec = ob.funcall::<_, _, i64>("nsec", ())?;
+        let v = sec * 1_000_000_000 + nsec;
+        // TODO support time zone when possible
+        // https://github.com/pola-rs/polars/issues/9103
+        Ok(AnyValue::Datetime(v, TimeUnit::Nanoseconds, &None))
+    } else if ob.is_nil() {
+        Ok(AnyValue::Null.into())
+    } else if let Some(dict) = RHash::from_value(ob) {
+        let len = dict.len();
+        let mut keys = Vec::with_capacity(len);
+        let mut vals = Vec::with_capacity(len);
+        dict.foreach(|k: Value, v: Value| {
+            let key = String::try_convert(k)?;
+            let val = Wrap::<AnyValue>::try_convert(v)?.0;
+            let dtype = DataType::from(&val);
+            keys.push(Field::new(&key, dtype));
+            vals.push(val);
+            Ok(ForEach::Continue)
+        })?;
+        Ok(AnyValue::StructOwned(Box::new((vals, keys))))
+    } else if let Some(v) = RArray::from_value(ob) {
+        if v.is_empty() {
+            Ok(AnyValue::List(Series::new_empty("", &DataType::Null)))
+        } else {
+            let list = v;
+
+            let mut avs = Vec::with_capacity(25);
+            let mut iter = list.into_iter();
+
+            for item in (&mut iter).take(25) {
+                avs.push(Wrap::<AnyValue>::try_convert(item)?.0)
+            }
+
+            let (dtype, _n_types) =
+                any_values_to_supertype_and_n_dtypes(&avs).map_err(RbPolarsErr::from)?;
+
+            // push the rest
+            avs.reserve(list.len());
+            for item in iter {
+                avs.push(Wrap::<AnyValue>::try_convert(item)?.0)
+            }
+
+            let s = Series::from_any_values_and_dtype("", &avs, &dtype, true)
+                .map_err(RbPolarsErr::from)?;
+            Ok(AnyValue::List(s))
+        }
+    } else if ob.is_kind_of(crate::rb_modules::datetime()) {
+        let sec: i64 = ob.funcall("to_i", ())?;
+        let nsec: i64 = ob.funcall("nsec", ())?;
+        Ok(AnyValue::Datetime(
+            sec * 1_000_000_000 + nsec,
+            TimeUnit::Nanoseconds,
+            &None,
+        ))
+    } else if ob.is_kind_of(crate::rb_modules::date()) {
+        // convert to DateTime for UTC
+        let v = ob
+            .funcall::<_, _, Value>("to_datetime", ())?
+            .funcall::<_, _, Value>("to_time", ())?
+            .funcall::<_, _, i64>("to_i", ())?;
+        Ok(AnyValue::Date((v / 86400) as i32))
+    } else if ob.is_kind_of(crate::rb_modules::bigdecimal()) {
+        let (sign, digits, _, exp): (i8, String, i32, i32) = ob.funcall("split", ()).unwrap();
+        let (mut v, scale) = abs_decimal_from_digits(digits, exp).ok_or_else(|| {
+            RbPolarsErr::other("BigDecimal is too large to fit in Decimal128".into())
+        })?;
+        if sign < 0 {
+            // TODO better error
+            v = v.checked_neg().unwrap();
+        }
+        Ok(AnyValue::Decimal(v, scale))
+    } else {
+        Err(RbPolarsErr::other(format!(
+            "object type not supported {:?}",
+            ob
+        )))
     }
 }
 
