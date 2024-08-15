336ced9077e791e0306b0b569c0880bef70d0a96
Andrew Kane <andrew@ankane.org>
2024-08-15 14:13:31 -0700

    Fixed CI

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,6 @@
 require "bundler/setup"
 Bundler.require(:default)
 require "minitest/autorun"
-require "minitest/pride"
 require "active_record"
 
 logger = ActiveSupport::Logger.new(ENV["VERBOSE"] ? STDOUT : nil)
