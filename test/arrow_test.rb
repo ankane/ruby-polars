a17860b9286785f5fe2762213bea5d93bf196c20
Andrew Kane <andrew@ankane.org>
2024-11-23 10:32:33 -0800

    Improved test [skip ci]

--- test/arrow_test.rb
+++ test/arrow_test.rb
@@ -3,7 +3,9 @@ require_relative "test_helper"
 class ArrowTest < Minitest::Test
   def test_c_stream
     df = Polars::DataFrame.new({"a" => [1, 2, 3], "b" => ["one", "two", "three"]})
+
     stream = df.arrow_c_stream
+    assert_kind_of Polars::ArrowArrayStream, stream
     assert_kind_of Integer, stream.to_i
 
     assert_frame df, Polars::DataFrame.new(stream)
