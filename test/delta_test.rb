a75a53f3d5f48a59c180b26cadf2878300fb2cc9
Andrew Kane <andrew@ankane.org>
2024-12-03 14:48:00 -0800

    Improved delta tests [skip ci]

--- test/delta_test.rb
+++ test/delta_test.rb
@@ -6,30 +6,33 @@ class DeltaTest < Minitest::Test
   end
 
   def test_read_delta
+    df = Polars.read_delta("test/support/delta")
+    expected = Polars::DataFrame.new({"a" => [1, 2, 3]})
+    assert_frame expected, df
+  end
+
+  def test_read_delta_table
+    df = Polars.read_delta(DeltaLake::Table.new("test/support/delta"))
+    expected = Polars::DataFrame.new({"a" => [1, 2, 3]})
+    assert_frame expected, df
+  end
+
+  def test_scan_delta
+    lf = Polars.scan_delta("test/support/delta")
+    expected = Polars::DataFrame.new({"a" => [1, 2, 3]})
+    assert_frame expected, lf.collect
+  end
+
+  def test_scan_delta_table
+    lf = Polars.scan_delta(DeltaLake::Table.new("test/support/delta"))
+    expected = Polars::DataFrame.new({"a" => [1, 2, 3]})
+    assert_frame expected, lf.collect
+  end
+
+  def test_write_delta
     df = Polars::DataFrame.new({"a" => [1, 2, 3]})
     path = temp_path
     assert_nil df.write_delta(path)
     assert_equal df, Polars.read_delta(path)
-  end
-
-  def test_read_delta_table
-    df = Polars::DataFrame.new({"a" => [1, 2, 3]})
-    path = temp_path
-    assert_nil df.write_delta(path)
-    assert_equal df, Polars.read_delta(DeltaLake::Table.new(path))
-  end
-
-  def test_scan_delta
-    df = Polars::DataFrame.new({"a" => [1, 2, 3]})
-    path = temp_path
-    assert_nil df.write_delta(path)
-    assert_equal df, Polars.scan_delta(path).collect
-  end
-
-  def test_scan_delta_table
-    df = Polars::DataFrame.new({"a" => [1, 2, 3]})
-    path = temp_path
-    assert_nil df.write_delta(path)
-    assert_equal df, Polars.scan_delta(DeltaLake::Table.new(path)).collect
   end
 end
