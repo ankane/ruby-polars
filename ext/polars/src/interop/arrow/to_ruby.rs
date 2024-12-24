a561eecab37cfb5b2d3fbe09f6c30326ded76fc1
Andrew Kane <andrew@ankane.org>
2024-12-24 09:51:05 -0800

    Updated compat level for arrow_c_stream method [skip ci]

--- ext/polars/src/interop/arrow/to_ruby.rs
+++ ext/polars/src/interop/arrow/to_ruby.rs
@@ -36,7 +36,7 @@ pub struct DataFrameStreamIterator {
 
 impl DataFrameStreamIterator {
     fn new(df: &DataFrame) -> Self {
-        let schema = df.schema().to_arrow(CompatLevel::oldest());
+        let schema = df.schema().to_arrow(CompatLevel::newest());
         let dtype = ArrowDataType::Struct(schema.into_iter_values().collect());
 
         Self {
@@ -67,7 +67,7 @@ impl Iterator for DataFrameStreamIterator {
             let batch_cols = self
                 .columns
                 .iter()
-                .map(|s| s.to_arrow(self.idx, CompatLevel::oldest()))
+                .map(|s| s.to_arrow(self.idx, CompatLevel::newest()))
                 .collect::<Vec<_>>();
             self.idx += 1;
 
