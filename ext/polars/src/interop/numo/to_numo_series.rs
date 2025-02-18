1591ad042cece4e93341b7b77f96d952b0bfe3b6
Andrew Kane <andrew@ankane.org>
2025-02-17 17:19:34 -0800

    Synced code [skip ci]

--- ext/polars/src/interop/numo/to_numo_series.rs
+++ ext/polars/src/interop/numo/to_numo_series.rs
@@ -8,59 +8,69 @@ use crate::series::RbSeries;
 use crate::RbResult;
 
 impl RbSeries {
-    /// For numeric types, this should only be called for Series with null types.
-    /// This will cast to floats so that `nil = NAN`
+    /// Convert this Series to a Numo array.
     pub fn to_numo(&self) -> RbResult<Value> {
-        let s = &self.series.borrow();
-        match s.dtype() {
-            DataType::String => {
-                let ca = s.str().unwrap();
+        series_to_numo(&self.series.borrow())
+    }
+}
 
+/// Convert a Series to a Numo array.
+fn series_to_numo(s: &Series) -> RbResult<Value> {
+    series_to_numo_with_copy(s)
+}
+
+/// Convert a Series to a Numo array, copying data in the process.
+fn series_to_numo_with_copy(s: &Series) -> RbResult<Value> {
+    use DataType::*;
+    match s.dtype() {
+        String => {
+            let ca = s.str().unwrap();
+
+            // TODO make more efficient
+            let np_arr = RArray::from_iter(ca);
+            class::object()
+                .const_get::<_, RModule>("Numo")?
+                .const_get::<_, RClass>("RObject")?
+                .funcall("cast", (np_arr,))
+        }
+        dt if dt.is_primitive_numeric() => {
+            if let Some(BitRepr::Large(_)) = s.bit_repr() {
+                let s = s.cast(&DataType::Float64).unwrap();
+                let ca = s.f64().unwrap();
                 // TODO make more efficient
-                let np_arr = RArray::from_iter(ca);
+                let np_arr = RArray::from_iter(ca.into_iter().map(|opt_v| match opt_v {
+                    Some(v) => v,
+                    None => f64::NAN,
+                }));
                 class::object()
                     .const_get::<_, RModule>("Numo")?
-                    .const_get::<_, RClass>("RObject")?
+                    .const_get::<_, RClass>("DFloat")?
+                    .funcall("cast", (np_arr,))
+            } else {
+                let s = s.cast(&DataType::Float32).unwrap();
+                let ca = s.f32().unwrap();
+                // TODO make more efficient
+                let np_arr = RArray::from_iter(ca.into_iter().map(|opt_v| match opt_v {
+                    Some(v) => v,
+                    None => f32::NAN,
+                }));
+                class::object()
+                    .const_get::<_, RModule>("Numo")?
+                    .const_get::<_, RClass>("SFloat")?
                     .funcall("cast", (np_arr,))
             }
-            dt if dt.is_primitive_numeric() => {
-                if let Some(BitRepr::Large(_)) = s.bit_repr() {
-                    let s = s.cast(&DataType::Float64).unwrap();
-                    let ca = s.f64().unwrap();
-                    // TODO make more efficient
-                    let np_arr = RArray::from_iter(ca.into_iter().map(|opt_v| match opt_v {
-                        Some(v) => v,
-                        None => f64::NAN,
-                    }));
-                    class::object()
-                        .const_get::<_, RModule>("Numo")?
-                        .const_get::<_, RClass>("DFloat")?
-                        .funcall("cast", (np_arr,))
-                } else {
-                    let s = s.cast(&DataType::Float32).unwrap();
-                    let ca = s.f32().unwrap();
-                    // TODO make more efficient
-                    let np_arr = RArray::from_iter(ca.into_iter().map(|opt_v| match opt_v {
-                        Some(v) => v,
-                        None => f32::NAN,
-                    }));
-                    class::object()
-                        .const_get::<_, RModule>("Numo")?
-                        .const_get::<_, RClass>("SFloat")?
-                        .funcall("cast", (np_arr,))
-                }
-            }
-            DataType::Boolean => boolean_series_to_numo(s),
-            dt => {
-                raise_err!(
-                    format!("'to_numo' not supported for dtype: {dt:?}"),
-                    ComputeError
-                );
-            }
+        }
+        Boolean => boolean_series_to_numo(s),
+        dt => {
+            raise_err!(
+                format!("'to_numo' not supported for dtype: {dt:?}"),
+                ComputeError
+            );
         }
     }
 }
 
+/// Convert booleans to bit if no nulls are present, otherwise convert to objects.
 fn boolean_series_to_numo(s: &Series) -> RbResult<Value> {
     let ca = s.bool().unwrap();
     if s.null_count() == 0 {
