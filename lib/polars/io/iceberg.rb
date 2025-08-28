e488c1bad528e5aa63f6738f896c71dc9d348a6c
Andrew Kane <andrew@ankane.org>
2025-08-27 18:50:41 -0700

    Added snapshot_id option to scan_iceberg method [skip ci]

--- lib/polars/io/iceberg.rb
+++ lib/polars/io/iceberg.rb
@@ -2,18 +2,20 @@ module Polars
   module IO
     # Lazily read from an Apache Iceberg table.
     #
-    # @param source
+    # @param source [Object]
     #   A Iceberg Ruby table, or a direct path to the metadata.
+    # @param snapshot_id [Integer]
+    #   The snapshot ID to scan from.
     #
     # @return [LazyFrame]
-    def scan_iceberg(source)
+    def scan_iceberg(source, snapshot_id: nil)
       require "iceberg"
 
       unless source.is_a?(Iceberg::Table)
         raise Todo
       end
 
-      source.to_polars
+      source.to_polars(snapshot_id:)
     end
   end
 end
