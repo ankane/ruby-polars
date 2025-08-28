282c716e3de0088dfdb19fdc516f27f63fc31071
Andrew Kane <andrew@ankane.org>
2025-08-27 18:20:55 -0700

    Improved Iceberg test [skip ci]

--- test/iceberg_test.rb
+++ test/iceberg_test.rb
@@ -8,11 +8,13 @@ class IcebergTest < Minitest::Test
     catalog.drop_table("polars_ruby_test.events", if_exists: true)
   end
 
-  def test_write_iceberg
-    df = Polars::DataFrame.new({"a" => [1, 2, 3], "b" => [4, 5, 6]})
+  def test_write_iceberg_mode_append
+    df = Polars::DataFrame.new({"a" => [1, 2, 3], "b" => [true, false, true]})
+    df2 = Polars::DataFrame.new({"a" => [4, 5, 6], "b" => [false, false, true]})
     table = catalog.create_table("polars_ruby_test.events", schema: df.schema)
     assert_nil df.write_iceberg(table, mode: "append")
-    assert_equal df, Polars.scan_iceberg(table).collect
+    assert_nil df2.write_iceberg(table, mode: "append")
+    assert_equal Polars.concat([df, df2]), Polars.scan_iceberg(table).collect
   end
 
   private
