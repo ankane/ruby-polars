a39c8041049ffd1b89e23c6f88423c901479bd30
Andrew Kane <andrew@ankane.org>
2025-09-03 22:11:03 -0700

    Improved QueryOptFlags class [skip ci]

--- lib/polars/query_opt_flags.rb
+++ lib/polars/query_opt_flags.rb
@@ -5,8 +5,46 @@ module Polars
   #   This functionality is considered **unstable**. It may be changed
   #   at any point without it being considered a breaking change.
   class QueryOptFlags
-    def initialize
+    def initialize(
+      predicate_pushdown: nil,
+      projection_pushdown: nil,
+      simplify_expression: nil,
+      slice_pushdown: nil,
+      comm_subplan_elim: nil,
+      comm_subexpr_elim: nil,
+      cluster_with_columns: nil,
+      collapse_joins: nil,
+      check_order_observe: nil,
+      fast_projection: nil
+    )
       @_rboptflags = RbOptFlags.default
+      update(
+        predicate_pushdown: predicate_pushdown,
+        projection_pushdown: projection_pushdown,
+        simplify_expression: simplify_expression,
+        slice_pushdown: slice_pushdown,
+        comm_subplan_elim: comm_subplan_elim,
+        comm_subexpr_elim: comm_subexpr_elim,
+        cluster_with_columns: cluster_with_columns,
+        collapse_joins: collapse_joins,
+        check_order_observe: check_order_observe,
+        fast_projection: fast_projection
+      )
+    end
+
+    def update(
+      predicate_pushdown: nil,
+      projection_pushdown: nil,
+      simplify_expression: nil,
+      slice_pushdown: nil,
+      comm_subplan_elim: nil,
+      comm_subexpr_elim: nil,
+      cluster_with_columns: nil,
+      collapse_joins: nil,
+      check_order_observe: nil,
+      fast_projection: nil
+    )
+      raise Todo
     end
   end
 end
