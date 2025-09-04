0a8a99c4661840ecab37d8d5faf2094d500b5f58
Andrew Kane <andrew@ankane.org>
2025-09-04 02:23:51 -0700

    Moved Active Record test setup to separate file [skip ci]

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,52 +1,8 @@
 require "bundler/setup"
 Bundler.require(:default)
 require "minitest/autorun"
-require "active_record"
 
-logger = ActiveSupport::Logger.new(ENV["VERBOSE"] ? STDOUT : nil)
-
-ActiveRecord::Base.logger = logger
-ActiveRecord::Migration.verbose = ENV["VERBOSE"]
-
-case ENV["ADAPTER"]
-when "postgresql"
-  ActiveRecord::Base.establish_connection adapter: "postgresql", database: "polars_ruby_test"
-when "mysql"
-  ActiveRecord::Base.establish_connection adapter: "mysql2", database: "polars_ruby_test"
-when "trilogy"
-  ActiveRecord::Base.establish_connection adapter: "trilogy", database: "polars_ruby_test", host: "127.0.0.1"
-else
-  ActiveRecord::Base.establish_connection adapter: "sqlite3", database: ":memory:"
-end
-
-if ActiveSupport::VERSION::STRING.to_f == 8.0
-  ActiveSupport.to_time_preserves_timezone = :zone
-elsif ActiveSupport::VERSION::STRING.to_f == 7.2
-  ActiveSupport.to_time_preserves_timezone = true
-end
-
-ActiveRecord::Schema.define do
-  create_table :users, force: true do |t|
-    t.string :name
-    t.integer :number
-    t.float :inexact
-    t.boolean :active
-    t.datetime :joined_at
-    t.date :joined_on
-    t.binary :bin
-    t.decimal :dec, precision: 10, scale: 3
-    t.text :txt
-    t.time :joined_time
-    if ENV["ADAPTER"] == "postgresql"
-      t.column :settings, :jsonb
-    else
-      t.column :settings, :json
-    end
-  end
-end
-
-class User < ActiveRecord::Base
-end
+require_relative "support/active_record"
 
 class Minitest::Test
   include Polars::Testing
