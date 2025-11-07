3c5a1de129723774773bebf17153651b1b6179aa
Andrew Kane <andrew@ankane.org>
2025-11-07 15:44:48 -0800

    Added rolling_std_by method to Series [skip ci]

--- lib/polars/series.rb
+++ lib/polars/series.rb
@@ -4250,6 +4250,93 @@ module Polars
       super
     end
 
+    # Compute a rolling standard deviation based on another series.
+    #
+    # @note
+    #   This functionality is considered **unstable**. It may be changed
+    #   at any point without it being considered a breaking change.
+    #
+    # Given a `by` column `<t_0, t_1, ..., t_n>`, then `closed="right"`
+    # (the default) means the windows will be:
+    #
+    #   - (t_0 - window_size, t_0]
+    #   - (t_1 - window_size, t_1]
+    #   - ...
+    #   - (t_n - window_size, t_n]
+    #
+    # @param by [Object]
+    #   Should be `DateTime`, `Date`, `UInt64`, `UInt32`, `Int64`,
+    #   or `Int32` data type (note that the integral ones require using `'i'`
+    #   in `window size`).
+    # @param window_size [String]
+    #   The length of the window. Can be a dynamic temporal
+    #   size indicated by a timedelta or the following string language:
+    #
+    #   - 1ns   (1 nanosecond)
+    #   - 1us   (1 microsecond)
+    #   - 1ms   (1 millisecond)
+    #   - 1s    (1 second)
+    #   - 1m    (1 minute)
+    #   - 1h    (1 hour)
+    #   - 1d    (1 calendar day)
+    #   - 1w    (1 calendar week)
+    #   - 1mo   (1 calendar month)
+    #   - 1q    (1 calendar quarter)
+    #   - 1y    (1 calendar year)
+    #   - 1i    (1 index count)
+    #
+    #   By "calendar day", we mean the corresponding time on the next day
+    #   (which may not be 24 hours, due to daylight savings). Similarly for
+    #   "calendar week", "calendar month", "calendar quarter", and
+    #   "calendar year".
+    # @param min_periods [Integer]
+    #   The number of values in the window that should be non-null before computing
+    #   a result.
+    # @param closed ['left', 'right', 'both', 'none']
+    #   Define which sides of the temporal interval are closed (inclusive),
+    #   defaults to `'right'`.
+    # @param ddof [Integer]
+    #   "Delta Degrees of Freedom": The divisor for a length N window is N - ddof
+    #
+    # @return [Series]
+    #
+    # @note
+    #   If you want to compute multiple aggregation statistics over the same dynamic
+    #   window, consider using `rolling` - this method can cache the window size
+    #   computation.
+    #
+    # @example
+    #   start = DateTime.new(2001, 1, 1)
+    #   stop = DateTime.new(2001, 1, 2)
+    #   s = Polars::Series.new("index", 25.times.to_a)
+    #   d = Polars::Series.new("date", Polars.datetime_range(start, stop, "1h", eager: true))
+    #   s.rolling_std_by(d, "3h")
+    #   # =>
+    #   # shape: (25,)
+    #   # Series: 'index' [f64]
+    #   # [
+    #   #         null
+    #   #         0.707107
+    #   #         1.0
+    #   #         1.0
+    #   #         1.0
+    #   #         …
+    #   #         1.0
+    #   #         1.0
+    #   #         1.0
+    #   #         1.0
+    #   #         1.0
+    #   # ]
+    def rolling_std_by(
+      by,
+      window_size,
+      min_periods: 1,
+      closed: "right",
+      ddof: 1
+    )
+      super
+    end
+
     # Compute a rolling std dev.
     #
     # A window of length `window_size` will traverse the array. The values that fill
