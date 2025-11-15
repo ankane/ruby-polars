a4d507ac7afe954d28df5b0714ce093e49243402
Andrew Kane <andrew@ankane.org>
2025-11-14 21:25:02 -0800

    Added from_records method to Polars [skip ci]

--- lib/polars/convert.rb
+++ lib/polars/convert.rb
@@ -134,7 +134,81 @@ module Polars
       )
     end
 
-    # def from_records
-    # end
+    # Construct a DataFrame from a sequence of sequences. This operation clones data.
+    #
+    # Note that this is slower than creating from columnar memory.
+    #
+    # @param data [Array]
+    #   Two-dimensional data represented as a sequence of sequences.
+    # @param schema [Object]
+    #   The DataFrame schema may be declared in several ways:
+    #
+    #   * As a dict of \\\\{name:type} pairs; if type is None, it will be auto-inferred.
+    #   * As a list of column names; in this case types are automatically inferred.
+    #   * As a list of (name,type) pairs; this is equivalent to the dictionary form.
+    #
+    #   If you supply a list of column names that does not match the names in the
+    #   underlying data, the names given here will overwrite them. The number
+    #   of names given in the schema should match the underlying data dimensions.
+    # @param schema_overrides [Hash]
+    #   Support type specification or override of one or more columns; note that
+    #   any dtypes inferred from the columns param will be overridden.
+    # @param strict [Boolean]
+    #   Throw an error if any `data` value does not exactly match the given or inferred
+    #   data type for that column. If set to `False`, values that do not match the data
+    #   type are cast to that data type or, if casting is not possible, set to null
+    #   instead.
+    # @param orient ['col', 'row']
+    #   Whether to interpret two-dimensional data as columns or as rows. If None,
+    #   the orientation is inferred by matching the columns and data dimensions. If
+    #   this does not yield conclusive results, column orientation is used.
+    # @param infer_schema_length [Integer]
+    #   The maximum number of rows to scan for schema inference.
+    #   If set to `nil`, the full data may be scanned *(this is slow)*.
+    #
+    # @return [DataFrame]
+    #
+    # @example
+    #   data = [[1, 2, 3], [4, 5, 6]]
+    #   Polars.from_records(data, schema: ["a", "b"])
+    #   # =>
+    #   # shape: (3, 2)
+    #   # ┌─────┬─────┐
+    #   # │ a   ┆ b   │
+    #   # │ --- ┆ --- │
+    #   # │ i64 ┆ i64 │
+    #   # ╞═════╪═════╡
+    #   # │ 1   ┆ 4   │
+    #   # │ 2   ┆ 5   │
+    #   # │ 3   ┆ 6   │
+    #   # └─────┴─────┘
+    def from_records(
+      data,
+      schema: nil,
+      schema_overrides: nil,
+      strict: true,
+      orient: nil,
+      infer_schema_length: N_INFER_DEFAULT
+    )
+      if !data.is_a?(::Array)
+        msg = (
+          "expected data of type Sequence, got #{data.class.name.inspect}" +
+          "\n\nHint: Try passing your data to the DataFrame constructor instead," +
+          " e.g. `Polars::DataFrame.new(data)`."
+        )
+        raise TypeError, msg
+      end
+
+      Utils.wrap_df(
+        DataFrame.sequence_to_rbdf(
+          data,
+          schema: schema,
+          schema_overrides: schema_overrides,
+          strict: strict,
+          orient: orient,
+          infer_schema_length: infer_schema_length
+        )
+      )
+    end
   end
 end
