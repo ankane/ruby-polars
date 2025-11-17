900c8fb7e105fd03cf67e3211306778f17207ab2
Andrew Kane <andrew@ankane.org>
2025-11-17 03:04:18 -0800

    Synced code [skip ci]

--- ext/polars/src/map/mod.rs
+++ ext/polars/src/map/mod.rs
@@ -17,10 +17,12 @@ impl RbPolarsNumericType for UInt8Type {}
 impl RbPolarsNumericType for UInt16Type {}
 impl RbPolarsNumericType for UInt32Type {}
 impl RbPolarsNumericType for UInt64Type {}
+impl RbPolarsNumericType for UInt128Type {}
 impl RbPolarsNumericType for Int8Type {}
 impl RbPolarsNumericType for Int16Type {}
 impl RbPolarsNumericType for Int32Type {}
 impl RbPolarsNumericType for Int64Type {}
+impl RbPolarsNumericType for Int128Type {}
 impl RbPolarsNumericType for Float32Type {}
 impl RbPolarsNumericType for Float64Type {}
 
