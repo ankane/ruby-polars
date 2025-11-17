f635fce4363f783d96c5d7dca45011aed6af7d71
Andrew Kane <andrew@ankane.org>
2025-11-16 17:24:29 -0800

    Improved code [skip ci]

--- ext/polars/src/exceptions.rs
+++ ext/polars/src/exceptions.rs
@@ -16,22 +16,8 @@ macro_rules! create_exception {
             }
         }
     };
-
-    ($type:ident, $cls:expr) => {
-        pub struct $type {}
-
-        impl $type {
-            pub fn new_err<T>(message: T) -> Error
-            where
-                T: Into<Cow<'static, str>>,
-            {
-                Error::new($cls, message)
-            }
-        }
-    };
 }
 
-// Errors
 create_exception!(AssertionError);
 create_exception!(ColumnNotFoundError);
 create_exception!(ComputeError);
@@ -47,11 +33,23 @@ create_exception!(ShapeError);
 create_exception!(StringCacheMismatchError);
 create_exception!(StructFieldNotFoundError);
 
-// Ruby errors
-create_exception!(RbTypeError, Ruby::get().unwrap().exception_type_error());
-create_exception!(RbValueError, Ruby::get().unwrap().exception_arg_error());
-create_exception!(
-    RbOverflowError,
-    Ruby::get().unwrap().exception_range_error()
-);
-create_exception!(RbIndexError, Ruby::get().unwrap().exception_index_error());
+macro_rules! create_ruby_exception {
+    ($type:ident, $cls:ident) => {
+        pub struct $type {}
+
+        impl $type {
+            pub fn new_err<T>(message: T) -> Error
+            where
+                T: Into<Cow<'static, str>>,
+            {
+                let cls = Ruby::get().unwrap().$cls();
+                Error::new(cls, message)
+            }
+        }
+    };
+}
+
+create_ruby_exception!(RbTypeError, exception_type_error);
+create_ruby_exception!(RbValueError, exception_arg_error);
+create_ruby_exception!(RbOverflowError, exception_range_error);
+create_ruby_exception!(RbIndexError, exception_index_error);
