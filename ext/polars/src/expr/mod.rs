15c78e9e4361c6becbf0e9fb86db01bc3d05ebef
Andrew Kane <andrew@ankane.org>
2025-11-16 23:46:00 -0800

    Synced code [skip ci]

--- ext/polars/src/expr/mod.rs
+++ ext/polars/src/expr/mod.rs
@@ -32,6 +32,16 @@ impl From<Expr> for RbExpr {
     }
 }
 
+pub(crate) trait ToExprs {
+    fn to_exprs(self) -> RbResult<Vec<Expr>>;
+}
+
+impl ToExprs for RArray {
+    fn to_exprs(self) -> RbResult<Vec<Expr>> {
+        rb_exprs_to_exprs(self)
+    }
+}
+
 pub fn rb_exprs_to_exprs(rb_exprs: RArray) -> RbResult<Vec<Expr>> {
     let mut exprs = Vec::new();
     for item in rb_exprs.into_iter() {
--- ext/polars/src/functions/aggregation.rs
+++ ext/polars/src/functions/aggregation.rs
@@ -1,41 +1,42 @@
 use magnus::RArray;
 use polars::lazy::dsl;
 
-use crate::rb_exprs_to_exprs;
-use crate::{RbExpr, RbPolarsErr, RbResult};
+use crate::error::RbPolarsErr;
+use crate::expr::ToExprs;
+use crate::{RbExpr, RbResult};
 
 pub fn all_horizontal(exprs: RArray) -> RbResult<RbExpr> {
-    let exprs = rb_exprs_to_exprs(exprs)?;
+    let exprs = exprs.to_exprs()?;
     let e = dsl::all_horizontal(exprs).map_err(RbPolarsErr::from)?;
     Ok(e.into())
 }
 
 pub fn any_horizontal(exprs: RArray) -> RbResult<RbExpr> {
-    let exprs = rb_exprs_to_exprs(exprs)?;
+    let exprs = exprs.to_exprs()?;
     let e = dsl::any_horizontal(exprs).map_err(RbPolarsErr::from)?;
     Ok(e.into())
 }
 
 pub fn max_horizontal(exprs: RArray) -> RbResult<RbExpr> {
-    let exprs = rb_exprs_to_exprs(exprs)?;
+    let exprs = exprs.to_exprs()?;
     let e = dsl::max_horizontal(exprs).map_err(RbPolarsErr::from)?;
     Ok(e.into())
 }
 
 pub fn min_horizontal(exprs: RArray) -> RbResult<RbExpr> {
-    let exprs = rb_exprs_to_exprs(exprs)?;
+    let exprs = exprs.to_exprs()?;
     let e = dsl::min_horizontal(exprs).map_err(RbPolarsErr::from)?;
     Ok(e.into())
 }
 
 pub fn sum_horizontal(exprs: RArray, ignore_nulls: bool) -> RbResult<RbExpr> {
-    let exprs = rb_exprs_to_exprs(exprs)?;
+    let exprs = exprs.to_exprs()?;
     let e = dsl::sum_horizontal(exprs, ignore_nulls).map_err(RbPolarsErr::from)?;
     Ok(e.into())
 }
 
 pub fn mean_horizontal(exprs: RArray, ignore_nulls: bool) -> RbResult<RbExpr> {
-    let exprs = rb_exprs_to_exprs(exprs)?;
+    let exprs = exprs.to_exprs()?;
     let e = dsl::mean_horizontal(exprs, ignore_nulls).map_err(RbPolarsErr::from)?;
     Ok(e.into())
 }
