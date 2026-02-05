d0224fd0e81c2eaccc72361c1f5d99223fb03e99
Andrew Kane <andrew@ankane.org>
2026-02-04 21:57:20 -0800

    Updated allocators to fix release workflow [skip ci]

--- ext/polars/src/c_api/allocator.rs
+++ ext/polars/src/c_api/allocator.rs
@@ -1,7 +1,7 @@
-#[cfg(target_family = "unix")]
+#[cfg(target_os = "linux")]
 #[global_allocator]
 static ALLOC: tikv_jemallocator::Jemalloc = tikv_jemallocator::Jemalloc;
 
-#[cfg(not(target_family = "unix"))]
+#[cfg(target_os = "macos")]
 #[global_allocator]
 static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
