c94afbcf0354bc4c1d1e8a0d103b8f06c9ce5ad9
Andrew Kane <andrew@ankane.org>
2026-07-21 21:21:42 -0700

    Improved Arrow tests [skip ci]

--- test/arrow_test.rb
+++ test/arrow_test.rb
@@ -4,37 +4,40 @@ class ArrowTest < Minitest::Test
   def test_series_from_arrow
     require "nanoarrow"
 
-    arr = Nanoarrow::Array.new([1, 2, 3], Nanoarrow.int64)
+    arr = Nanoarrow::Array.new([1, nil, 3], Nanoarrow.int64)
     s = Polars::Series.new(arr)
-    assert_series [1, 2, 3], s, dtype: Polars::Int64
+    assert_series [1, nil, 3], s, dtype: Polars::Int64
   end
 
   def test_series_to_arrow
-    s = Polars::Series.new("a", [1, 2, 3])
+    s = Polars::Series.new("a", [1, nil, 3])
     arr = s.to_arrow
     assert_kind_of Nanoarrow::Array, arr
     assert_equal "int64", arr.schema.type
-    assert_equal [1, 2, 3], arr.to_a
+    assert_equal [1, nil, 3], arr.to_a
   end
 
   def test_data_frame_from_arrow
-    df = Polars::DataFrame.new({"a" => [1, 2, 3], "b" => ["one", "two", "three"]})
-    arr = df.to_arrow
-    assert_frame_equal df, Polars::DataFrame.new(arr)
+    require "nanoarrow"
+
+    rows = [{"a" => 1, "b" => "one"}, {"a" => 2, "b" => nil}, {"a" => nil, "b" => "three"}]
+    arr = Nanoarrow::Array.new(rows, Nanoarrow.struct({"a" => Nanoarrow.int64, "b" => Nanoarrow.string}))
+    assert_equal rows, Polars::DataFrame.new(arr).to_a
   end
 
   def test_data_frame_to_arrow
-    df = Polars::DataFrame.new({"a" => [1, 2, 3], "b" => ["one", "two", "three"]})
+    df = Polars::DataFrame.new({"a" => [1, 2, nil], "b" => ["one", nil, "three"]})
     arr = df.to_arrow
     assert_kind_of Nanoarrow::Array, arr
-    assert_equal [1, 2, 3], arr.child(0).to_a
-    assert_equal ["one", "two", "three"], arr.child(1).to_a
+    assert_equal [1, 2, nil], arr.child(0).to_a
+    assert_equal ["one", nil, "three"], arr.child(1).to_a
   end
 
   def test_schema_from_arrow
-    df = Polars::DataFrame.new({"a" => [1, 2, 3], "b" => ["one", "two", "three"]})
-    schema = df.schema.to_arrow
-    assert_equal df.schema.to_h, Polars::Schema.new(schema).to_h
+    require "nanoarrow"
+
+    schema = Nanoarrow.struct({"a" => Nanoarrow.int64, "b" => Nanoarrow.string})
+    assert_equal ({"a" => Polars::Int64, "b" => Polars::String}), Polars::Schema.new(schema).to_h
 
     error = assert_raises(ArgumentError) do
       Polars::Schema.new(Nanoarrow.int64)
