bba638bd15ed9c06e45c0704841780c88d109002
Andrew Kane <andrew@ankane.org>
2026-08-05 03:19:35 -0700

    Synced code [skip ci]

--- ext/polars/src/dataframe/io.rs
+++ ext/polars/src/dataframe/io.rs
@@ -14,7 +14,7 @@ use crate::utils::EnterPolarsExt;
 use crate::{RbPolarsErr, RbResult};
 
 impl RbDataFrame {
-    pub fn read_csv(arguments: &[Value]) -> RbResult<Self> {
+    pub fn read_csv(rb: &Ruby, arguments: &[Value]) -> RbResult<Self> {
         // start arguments
         // this pattern is needed for more than 16
         let rb_f = arguments[0];
@@ -84,46 +84,47 @@ impl RbDataFrame {
 
         let rb_f = read_if_bytesio(rb_f);
         let mmap_bytes_r = get_mmap_bytes_reader(&rb_f)?;
-        let df = CsvReadOptions::default()
-            .with_path(path)
-            .with_infer_schema_length(infer_schema_length)
-            .with_has_header(has_header)
-            .with_n_rows(n_rows)
-            .with_skip_rows(skip_rows)
-            .with_skip_lines(skip_lines)
-            .with_ignore_errors(ignore_errors)
-            .with_projection(projection.map(Arc::new))
-            .with_rechunk(rechunk)
-            .with_chunk_size(chunk_size)
-            .with_columns(columns.map(|x| x.into_iter().map(|x| x.into()).collect()))
-            .with_n_threads(n_threads)
-            .with_schema_overwrite(overwrite_dtype.map(Arc::new))
-            .with_dtype_overwrite(overwrite_dtype_slice.map(Arc::new))
-            .with_schema(schema.map(|schema| Arc::new(schema.0)))
-            .with_low_memory(low_memory)
-            .with_skip_rows_after_header(skip_rows_after_header)
-            .with_row_index(row_index)
-            .with_raise_if_empty(raise_if_empty)
-            .with_parse_options(
-                CsvParseOptions::default()
-                    .with_separator(separator.as_bytes()[0])
-                    .with_encoding(encoding.0)
-                    .with_missing_is_null(!missing_utf8_is_empty_string)
-                    .with_comment_prefix(comment_prefix.as_deref())
-                    .with_null_values(null_values)
-                    .with_try_parse_dates(try_parse_dates)
-                    .with_quote_char(quote_char)
-                    .with_eol_char(eol_char)
-                    .with_truncate_ragged_lines(truncate_ragged_lines)
-                    .with_decimal_comma(decimal_comma),
-            )
-            .into_reader_with_file_handle(mmap_bytes_r)
-            .finish()
-            .map_err(RbPolarsErr::from)?;
-        Ok(df.into())
+        rb.enter_polars_df(move || {
+            CsvReadOptions::default()
+                .with_path(path)
+                .with_infer_schema_length(infer_schema_length)
+                .with_has_header(has_header)
+                .with_n_rows(n_rows)
+                .with_skip_rows(skip_rows)
+                .with_skip_lines(skip_lines)
+                .with_ignore_errors(ignore_errors)
+                .with_projection(projection.map(Arc::new))
+                .with_rechunk(rechunk)
+                .with_chunk_size(chunk_size)
+                .with_columns(columns.map(|x| x.into_iter().map(|x| x.into()).collect()))
+                .with_n_threads(n_threads)
+                .with_schema_overwrite(overwrite_dtype.map(Arc::new))
+                .with_dtype_overwrite(overwrite_dtype_slice.map(Arc::new))
+                .with_schema(schema.map(|schema| Arc::new(schema.0)))
+                .with_low_memory(low_memory)
+                .with_skip_rows_after_header(skip_rows_after_header)
+                .with_row_index(row_index)
+                .with_raise_if_empty(raise_if_empty)
+                .with_parse_options(
+                    CsvParseOptions::default()
+                        .with_separator(separator.as_bytes()[0])
+                        .with_encoding(encoding.0)
+                        .with_missing_is_null(!missing_utf8_is_empty_string)
+                        .with_comment_prefix(comment_prefix.as_deref())
+                        .with_null_values(null_values)
+                        .with_try_parse_dates(try_parse_dates)
+                        .with_quote_char(quote_char)
+                        .with_eol_char(eol_char)
+                        .with_truncate_ragged_lines(truncate_ragged_lines)
+                        .with_decimal_comma(decimal_comma),
+                )
+                .into_reader_with_file_handle(mmap_bytes_r)
+                .finish()
+        })
     }
 
     pub fn read_json(
+        rb: &Ruby,
         rb_f: Value,
         infer_schema_length: Option<usize>,
         schema: Option<Wrap<Schema>>,
@@ -132,20 +133,21 @@ impl RbDataFrame {
         let rb_f = read_if_bytesio(rb_f);
         let mmap_bytes_r = get_mmap_bytes_reader(&rb_f)?;
 
-        let mut builder = JsonReader::new(mmap_bytes_r)
-            .with_json_format(JsonFormat::Json)
-            .infer_schema_len(infer_schema_length.and_then(NonZeroUsize::new));
+        rb.enter_polars_df(move || {
+            let mut reader = JsonReader::new(mmap_bytes_r)
+                .with_json_format(JsonFormat::Json)
+                .infer_schema_len(infer_schema_length.and_then(NonZeroUsize::new));
 
-        if let Some(schema) = schema {
-            builder = builder.with_schema(Arc::new(schema.0));
-        }
+            if let Some(schema) = schema {
+                reader = reader.with_schema(Arc::new(schema.0));
+            }
 
-        if let Some(schema) = schema_overrides.as_ref() {
-            builder = builder.with_schema_overwrite(&schema.0);
-        }
+            if let Some(schema) = schema_overrides.as_ref() {
+                reader = reader.with_schema_overwrite(&schema.0);
+            }
 
-        let out = builder.finish().map_err(RbPolarsErr::from)?;
-        Ok(out.into())
+            reader.finish()
+        })
     }
 
     pub fn read_ipc(
